2e5e47acc100c4c8b77ce9778cce824cc1047be2
carpediem <mat.carpella@gmail.com>
2019-01-31 13:17:21 +0100

    port changed

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-var express = require('express')
-var app = express()
-app.listen(3000, () => {
-  console.log('Server running on port 3000')
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log('Server running on port ' + PORT)
 })
 
 app.get('/', (req, res, next) => {
